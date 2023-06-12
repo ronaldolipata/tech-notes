53bafe411e729c7a3fa4399e9e1b93942405fc6c
Ronaldo Lipata <lipata.ronaldo@gmail.com>
2023-06-12 14:32:09 +0800

    refac: logger structure - convert from function expression to function declaration to stay consistent - move the variables from logEvents inside the try catch - remove unused res from the logger and replace into _

--- backend/src/middlewares/logger.ts
+++ backend/src/middlewares/logger.ts
@@ -5,17 +5,18 @@ import { promises as fsPromises } from 'fs';
 import path from 'path';
 import { Request, Response, NextFunction } from 'express';
 
-const logEvents = async (
+export async function logEvents(
   message: string,
   logFileName: string
-): Promise<void> => {
-  const dateTime: string = format(new Date(), 'yyyyMMdd\tHH:mm:ss');
-  const logItem: string = `${dateTime}\t${uuid()}\t${message}\n`;
-
+): Promise<void> {
   try {
+    const dateTime: string = format(new Date(), 'yyyyMMdd\tHH:mm:ss');
+    const logItem: string = `${dateTime}\t${uuid()}\t${message}\n`;
+
     if (!fs.existsSync(path.join(__dirname, '..', '..', 'logs'))) {
       await fsPromises.mkdir(path.join(__dirname, '..', '..', 'logs'));
     }
+
     await fsPromises.appendFile(
       path.join(__dirname, '..', '..', 'logs', logFileName),
       logItem
@@ -23,15 +24,13 @@ const logEvents = async (
   } catch (error) {
     console.log(error);
   }
-};
+}
 
-const logger = (req: Request, res: Response, next: NextFunction): void => {
+export function logger(req: Request, _: Response, next: NextFunction): void {
   logEvents(
     `${req.method}\t${req.url}\t${req.headers.origin}`,
     'requestLog.log'
   );
   console.log(`${req.method} ${req.path}`);
   next();
-};
-
-export { logEvents, logger };
+}
